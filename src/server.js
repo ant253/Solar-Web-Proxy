daf4065064a877746a072e4c91fdeaa647d0745f
ant253 <antoni.laczny@gmail.com>
2025-02-15 17:33:07 +0000

    Version 1

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
+import { epoxyPath } from '@mercuryworkshop/epoxy-transport';
+import { baremuxPath } from '@mercuryworkshop/bare-mux/node';
+import { join } from 'node:path';
+import wisp from 'wisp-server-node';
+
+const app = express();
+const port = 3000;
+
+// Serve static files
+app.use(express.static(join(process.cwd(), 'public')));
+
+// Serve Ultraviolet files
+app.use('/uv/', express.static(uvPath));
+
+// Serve Epoxy and BareMux files
+app.use('/epoxy/', express.static(epoxyPath));
+app.use('/baremux/', express.static(baremuxPath));
+
+// Handle Wisp upgrades
+const server = createServer(app);
+server.on('upgrade', (req, socket, head) => {
+  if (req.url.endsWith('/wisp/')) {
+    wisp.routeRequest(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+// Run on local IP for port forwarding
+const localIP = '0.0.0.0'; // Listen on all interfaces
+server.listen(port, localIP, () => {
+  console.log(`Server running at http://${localIP}:${port}`);
+});
